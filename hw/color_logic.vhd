3bb3f2eb99e9c14f54d0cb98a6fd0cfae0cb0e69
Noah Dunleavy <noah.dunleavy@gmail.com>
2025-05-29 05:34:41 -0400

    Modified address

--- hw/color_logic.vhd
+++ hw/color_logic.vhd
@@ -24,16 +24,16 @@ end color_logic;
 
 architecture Behavioral of color_logic is
 
-signal red_int, green_int, blue_int : std_logic_vector(7 downto 0) := (others => '0');
+signal red_int, green_int, blue_int : unsigned(7 downto 0) := (others => '0');
+signal red_int_std, green_int_std, blue_int_std : std_logic_vector(7 downto 0) := (others => '0');
 
 begin
 
 
-red_int <= std_logic_vector(75 + y_in(Y_CURSOR_WIDTH - 1 downto 2));  --divide it by four
-green_int <= std_logic_vector(175 - y_in(Y_CURSOR_WIDTH - 1 downto 3) + x_in(X_CURSOR_WIDTH - 1 downto 4)); --div row by 8, col by 16
-blue_int <= std_logic_vector(255 - y_in(Y_CURSOR_WIDTH - 1 downto 3) + x_in(X_CURSOR_WIDTH - 1 downto 4)); --div row by 16, col by 16
+red_int   <= resize(to_unsigned(75, 8) + resize(y_in(Y_CURSOR_WIDTH - 1 downto 2), 8), 8);
+green_int <= resize(to_unsigned(175, 8) - resize(y_in(Y_CURSOR_WIDTH - 1 downto 3), 8) + resize(x_in(X_CURSOR_WIDTH - 1 downto 4), 8), 8);
+blue_int  <= resize(to_unsigned(255, 8) - resize(y_in(Y_CURSOR_WIDTH - 1 downto 3), 8) + resize(x_in(X_CURSOR_WIDTH - 1 downto 4), 8), 8);
 
-
-rgb_out <= red_int & green_int & blue_int;
+rgb_out <= std_logic_vector(red_int & green_int & blue_int);
 
 end Behavioral;
--- hw/zybo_z7-10_constraints.xdc
+++ hw/zybo_z7-10_constraints.xdc
@@ -36,7 +36,7 @@
 ## -----------------------------------------------------------------------------------------------------------------------------------------------------------
 ## Switches
 ## -----------------------------------------------------------------------------------------------------------------------------------------------------------
-#set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 			} [get_ports { fir_select_i[0]			}]; #IO_L19N_T3_VREF_35 		Sch=sw[0]
+set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 			} [get_ports { dbg_i			}]; #IO_L19N_T3_VREF_35 		Sch=sw[0]
 #set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 			} [get_ports { fir_select_i[1]			}]; #IO_L24P_T3_34 				Sch=sw[1]
 #set_property -dict { PACKAGE_PIN W13   IOSTANDARD LVCMOS33 			} [get_ports { fir_enable_i			}]; #IO_L4N_T0_34 				Sch=sw[2]
 #set_property -dict { PACKAGE_PIN T16   IOSTANDARD LVCMOS33 			} [get_ports { codec_or_dds_sel_i			}]; #IO_L9P_T1_DQS_34 			Sch=sw[3]
